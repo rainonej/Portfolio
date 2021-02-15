f028d8af4f9de32f8579b398973ab5275c1eb884
Jordan <rainonej@gmail.com>
2021-02-15 01:59:12 -0500

    Created a way to get new suggested searched

---
+++ get_suggestions.py
@@ -0,0 +1,16 @@
+import pytrends as pt
+from pytrends.request import TrendReq
+pytrends = TrendReq(hl='en-US', tz=360)
+
+
+kw_list = ["ETH-USD", "Ethereum"]
+pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
+
+kw_blist = kw_list
+q = pytrends.related_queries() 
+for i in q:
+	df = q[i]['top']
+	filt = (df['value'] > 50 )
+	kw_blist += list(df.loc[filt,'query'])
+
+kw_blist = list(set(kw_blist))
