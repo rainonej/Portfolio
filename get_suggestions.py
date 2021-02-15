b61c4ecb28edefe8832a65d9fff52bb670f0dd45
Jordan <rainonej@gmail.com>
2021-02-15 16:17:09 -0500

    suggestions works

--- get_suggestions.py
+++ get_suggestions.py
@@ -6,14 +6,14 @@ pytrends = TrendReq(hl='en-US', tz=360)
 kw_list = ["ETH-USD", "Ethereum"]
 
 def new_words(words):
-	pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
+	pytrends.build_payload(words, cat=0, timeframe='today 5-y', geo='', gprop='')
 
-	kw_blist = kw_list
+	kw_blist = words
 	q = pytrends.related_queries() 
-	print(q)
+	#print(q)
 	for i in q:
 		df = q[i]['top']
-		print(df.info())
+		#print(df.info())
 		filt = (df['value'] > 50 )
 		kw_blist += list(df.loc[filt,'query'])
 
