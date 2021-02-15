894d10daff0001d0385dfa908769b2ae3688a33b
Jordan <rainonej@gmail.com>
2021-02-15 16:35:54 -0500

    Second graph

--- create_graph.py
+++ create_graph.py
@@ -12,12 +12,19 @@ file_name =  SYMBL + key_word + str(start_year)[2:] + '-' + str(end_year)[2:]
 df = pd.read_pickle("./pickles/" + file_name + ".pkl")
 
 searches = df.keys()[0]
+stock_shift = df.keys()[1]
+
+df = df.rename(columns = {searches: key_word, stock_shift: 'Shifted Stock Price', 'Open': 'Stock Price'})
+searches = key_word
+stock_shift = 'Shifted Stock Price'
 plt.figure()
-df[searches].plot(label = 'testefwef')
-temp = 'Open-25'
+df[searches].plot(legend = True, ylabel = 'Google Searches')
+#temp = 'Open-25'
 #ax.set_ylabel('Google Searches')
 
-df['Open'].plot(secondary_y=True, style='g')
+df[stock_shift].plot(secondary_y=True, style='g', legend = True)
+#df.plot(secondary_y=True, style='g')
+
 
 file_name = SYMBL + '.png'
 plt.savefig(file_name)
