a72dc31c007f7f3dfef0b805ad3d19749ef8a53c
Jordan <rainonej@gmail.com>
2021-02-27 00:57:34 -0500

    get_google_trends is less verbose
    Made verbose an optional parameter for get_google_trends. Also added an 'update' parameter.
    In addition, the file can now run for a predetermined amount of time, or until it finishes.

--- get_google_trends.py
+++ get_google_trends.py
@@ -1,26 +1,64 @@
 import pandas as pd
 from pytrendsdaily import getDailyData
 from os import path
-from key_words import key_words, start_year, end_year
+from key_words import key_words, start_year, end_year, SYMBLs
 
 print('key words = ')
 print(key_words)
 
-for key in key_words:
-	for year in range(start_year, end_year+1):
-		filename = key + '_searches_' + str(year) + '.pkl'
-		if path.exists("pickles/" + filename):
-			print(filename + ' already exists')
-		else:
-			try:
-				search_trend = getDailyData(key, year, year, wait_time = 0)
-				search_trend = search_trend.reset_index()
-				search_trend = search_trend.rename(columns={"date": "Date"})
-				search_trend = search_trend.set_index('Date')
+import time
+timeout = time.time() + 60*5  # 5 minutes from now
 
-				search_trend.to_pickle("pickles/" + filename)
-				print(filename + ' stored')
-			except:
-				print('Google prevented ' + filename + ' from being stored')
+verbose = False
+updates = True
+update_time = 15 # 15 seconds
+
+while True:
+	if (failed == 0):
+		if (updates or verbose):
+			print('Finished!')
+			break
+
+	if time.time() > timeout:
+		if (updates or verbose):
+			print('Timed out.')
+		break
+
+	if updates: print('New cycle started')
+
+	succeeded = 0
+	failed = 0
+	update = time.time()
+
+	for key in key_words:
+		for year in range(start_year, end_year+1):
+			filename = key + '_searches_' + str(year) + '.pkl'
+			if path.exists("pickles/" + filename):
+				if verbose: print(filename + ' already exists')
+			else:
+				try:
+					search_trend = getDailyData(key, year, year, wait_time = 0, verbose = verbose)
+					search_trend = search_trend.reset_index()
+					search_trend = search_trend.rename(columns={"date": "Date"})
+					search_trend = search_trend.set_index('Date')
+
+					search_trend.to_pickle("pickles/" + filename)
+					if verbose: print(filename + ' stored')
+					succeeded += 1
+				except:
+					if verbose: print('Google prevented ' + filename + ' from being stored')
+					failed += 1
+
+			if updates:
+				if (time.time() > update):
+					update = time.time() + update_time
+					print(succeeded, ' succeeded, and ', failed, ' failed so far in this cycle')
+					mins = int((timeout - time.time())/60)
+					secs = int((timeout - time.time())%60)
+					print(mins, ':', secs, ' remain')
+					#print(int((timeout - time.time())/60), ' mins and ', int((timeout - time.time())%60), ' secs. remain')
+
+
+	if updates: print(succeeded, ' succeeded, and ', failed, ' failed this cycle')
 
 
--- scratch_pics.py
+++ scratch_pics.py
@@ -30,6 +30,17 @@ end_year = END_DATE[:4]
 price_df = pd.DataFrame({})
 searches_df = pd.DataFrame({})
 
+print('starting the loop')
+import time
+timeout = time.time() + 10  # 5 minutes from now
+test = 0
+while True:
+    test += 1
+    if time.time() > timeout:
+        break
+print('That took ', test, ' steps!')
+
+'''
 for year in range(int(start_year), int(end_year)+1):
 	price_file = "pickles/" + SYMBL + "_price_" + str(year) + ".pkl"
 	search_file = "pickles/" + KEY_WORD + "_searches_" + str(year) + ".pkl"
@@ -47,7 +58,7 @@ df = price_df['Open']
 #pd.plotting.lag_plot(df)
 pd.plotting.autocorrelation_plot(df)
 plt.show()
-
+'''
 '''
 fig = plt.figure(figsize = (5,5)) #Instantiate the Figure
 gs = fig.add_gridspec(1, 1) #Choose the grid size for the number of graphs
